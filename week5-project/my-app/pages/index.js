a2dfe7eae307af743d984f55e061644cf848071c
Rolando <tsdrolando@gmail.com>
2022-05-09 18:40:28 -0400

    check

--- week5-project/my-app/pages/index.js
+++ week5-project/my-app/pages/index.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <div>
-      <h1>Home page</h1>
+      <h1 className='text-xl font-bold'>Home page</h1>
       <Link href='/blog'>
         <a>Blog</a>
       </Link>
